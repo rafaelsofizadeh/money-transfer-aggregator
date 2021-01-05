9a8ffadfba4276f1c6b978e99f3052d869317796
rafaelsofizadeh <rafa.sofizadeh@gmail.com>
2021-01-06 01:21:43 +0400

    combine

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 import puppeteer from "puppeteer";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 import config from "./config.json";
 import inputProcessingMiddleware from "./middleware/processInputMiddleware.js";
