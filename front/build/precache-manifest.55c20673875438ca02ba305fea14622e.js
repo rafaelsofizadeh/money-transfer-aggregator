8f21d6ebfaad631e4aa1ac5f1aad51a553147521
rafaelsofizadeh <rafa.sofizadeh@gmail.com>
2021-01-06 21:36:43 +0400

    build

---
+++ front/build/precache-manifest.55c20673875438ca02ba305fea14622e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a5fb040f32528329f3451f1a3e26c4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "852d644d4f76f55cb4db",
+    "url": "/static/css/main.13d6b543.chunk.css"
+  },
+  {
+    "revision": "aab24fcb4128e7faa500",
+    "url": "/static/js/2.98c1d6e9.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.98c1d6e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7556519be794ee5a6bcd",
+    "url": "/static/js/3.9da3b787.chunk.js"
+  },
+  {
+    "revision": "852d644d4f76f55cb4db",
+    "url": "/static/js/main.0196c047.chunk.js"
+  },
+  {
+    "revision": "24b74dfdf70f785e277c",
+    "url": "/static/js/runtime-main.68d5a68c.js"
+  },
+  {
+    "revision": "a64d1dc87979701ff1d03a39f724196c",
+    "url": "/static/media/bank.a64d1dc8.png"
+  },
+  {
+    "revision": "d94847c167f0f39314200be731c84450",
+    "url": "/static/media/creditCard.d94847c1.png"
+  },
+  {
+    "revision": "2052c3df3101891c11766ae93f2b5ac2",
+    "url": "/static/media/moneyStack.2052c3df.png"
+  },
+  {
+    "revision": "21a1570bb1ca245faa96a62d5a22515e",
+    "url": "/static/media/red-low-poly-bg.21a1570b.jpg"
+  }
+]);
